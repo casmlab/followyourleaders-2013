3dc6bbb68ef752f227b53171f5d4e71840c5b3dc
eyoela <eyoela@gmail.com>
2013-05-25 19:28:41 +0300

    -fixing the code structure for us-congress and url pattern

--- fyl_try2/urls.py
+++ fyl_try2/urls.py
@@ -11,12 +11,6 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fyl_try2.views.home', name='home'),
     # url(r'^fyl_try2/', include('fyl_try2.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
     
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
     url(r'^$', 'fyl_try2.views.index'),
@@ -24,8 +18,9 @@ urlpatterns = patterns('',
     url(r'^about$', 'fyl_try2.views.about', name='about'),
 #    url(r'^contact$', 'fyl_try2.views.contact', name='contact'),
     url(r'^us-congress$', 'fyl_try2.views.get_us_congress', name='us-congress'),
-    url(r'^us-congress-political-map$', 'fyl_try2.views.us_congress_pltcl_map', name='us-congress-map'),
-    url(r'^us-congress-trends$', 'fyl_try2.views.us_congress_trends', name='us-congress-trends'),
+    url(r'^political-map$', 'fyl_try2.views.us_congress_pltcl_map', name='us-congress-map'),
+    url(r'^us-congress/trends$', 'fyl_try2.views.us_congress_trends', name='us-congress-trends'),
+    url(r'^ngrams$', 'fyl_try2.views.us_congress_trends', name='us-congress-trends'),
    
 )
 handler404 = 'fyl_try2.views.custom_404_view'
